4aef2396fd71be3ae030812415d97fdb225e1459
Oladuja Adeola Progress <40114094+oladuja@users.noreply.github.com>
2023-10-30 22:40:34 +0100

    loggedin widget

--- lib/views/start/login_screen.dart
+++ lib/views/start/login_screen.dart
@@ -10,148 +10,172 @@ import 'package:healthier_carbon_pregnancy_app/views/start/signup_screen.dart';
 import 'package:healthier_carbon_pregnancy_app/widgets/app_button.dart';
 import 'package:healthier_carbon_pregnancy_app/widgets/app_text_field.dart';
 import 'package:healthier_carbon_pregnancy_app/widgets/app_text_field_password.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:provider/provider.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const String routeName = 'login-screen';
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isLoggedInSelected = false;
   final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     CreateNewUser user = Provider.of<CreateNewUser>(context);
 
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            width: size.width,
-            height: size.height,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.2),
-                  BlendMode.darken,
-                ),
-                image: const AssetImage('assets/images/bg2.jpg'),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: () => Navigator.of(context)
-                          .popAndPushNamed(SignUpScreen.routeName),
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
+      body: isLoggedInSelected
+          ? LoadingAnimationWidget.staggeredDotsWave(
+              color: Colors.white,
+              size: 200,
+            )
+          : SafeArea(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: size.width,
+                  height: size.height,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.2),
+                        BlendMode.darken,
+                      ),
+                      image: const AssetImage('assets/images/bg2.jpg'),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 25, horizontal: 30),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: GestureDetector(
+                            onTap: () => Navigator.of(context)
+                                .popAndPushNamed(SignUpScreen.routeName),
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                  const Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Healthier Carbon',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const Spacer(),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Email',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  AppTextField(
-                    type: TextInputType.emailAddress,
-                    controller: email,
-                  ),
-                  const SizedBox(height: 35),
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Password',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  AppTextFieldPassword(controller: password),
-                  const SizedBox(height: 45),
-                  AppButton(
-                    text: "SIGN IN",
-                    onTap: () async {
-                      try {
-                        await Auth.account(
-                            email.text, password.text, AuthMode.login);
-                        await prefs.setBool('isUserLoggedIn', true);
-                        var data =
-                            await FireStore().getUser(auth.currentUser!.uid);
-                        user.setProfile(data.data()!);
-                        Navigator.of(context)
-                            .popAndPushNamed(HomeScreen.routeName);
-                      } on FirebaseAuthException catch (e) {
-                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(e.message!),
+                        const Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Healthier Carbon',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        );
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(e.toString()),
+                        ),
+                        const Spacer(),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Email',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
                           ),
-                        );
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 15),
-                  GestureDetector(
-                    onTap: () => Navigator.of(context)
-                        .pushNamed(ForgotPasswordScreen.routeName),
-                    child: const Text(
-                      'Forgot Password? Click HERE to recover',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+                        ),
+                        const SizedBox(height: 5),
+                        AppTextField(
+                          type: TextInputType.emailAddress,
+                          controller: email,
+                        ),
+                        const SizedBox(height: 35),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Password',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        AppTextFieldPassword(controller: password),
+                        const SizedBox(height: 45),
+                        AppButton(
+                          text: "SIGN IN",
+                          onTap: () async {
+                            setState(() {
+                              isLoggedInSelected = true;
+                            });
+                            try {
+                              await Auth.account(
+                                  email.text, password.text, AuthMode.login);
+                              await prefs.setBool('isUserLoggedIn', true);
+                              var data = await FireStore()
+                                  .getUser(auth.currentUser!.uid);
+                              user.setProfile(data.data()!);
+                              Navigator.of(context)
+                                  .popAndPushNamed(HomeScreen.routeName);
+                            } on FirebaseAuthException catch (e) {
+                              ScaffoldMessenger.of(context)
+                                  .hideCurrentSnackBar();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(e.message!),
+                                ),
+                              );
+                            } catch (e) {
+                              ScaffoldMessenger.of(context)
+                                  .hideCurrentSnackBar();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(e.toString()),
+                                ),
+                              );
+                            }
+                            setState(() {
+                              isLoggedInSelected = false;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        GestureDetector(
+                          onTap: () => Navigator.of(context)
+                              .pushNamed(ForgotPasswordScreen.routeName),
+                          child: const Text(
+                            'Forgot Password? Click HERE to recover',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/logo.png'),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 15),
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/logo.png'),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
--- lib/views/start/start_screen.dart
+++ lib/views/start/start_screen.dart
@@ -9,12 +9,19 @@ import 'package:healthier_carbon_pregnancy_app/views/start/signup_screen.dart';
 import 'package:healthier_carbon_pregnancy_app/views/start/stage_screen.dart';
 import 'package:healthier_carbon_pregnancy_app/widgets/app_button.dart';
 import 'package:healthier_carbon_pregnancy_app/widgets/sign_with_button.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   static const String routeName = 'start-screen';
 
   const StartScreen({super.key});
+
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  bool showLoadingAnimationWidget = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,87 +29,89 @@ class StartScreen extends StatelessWidget {
 
     return Scaffold(
       body: Container(
-        width: size.width,
-        height: size.height,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.2),
-              BlendMode.darken,
-            ),
-            image: const AssetImage('assets/images/bg4.jpg'),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 45),
-          child: Column(
-            children: [
-              const Spacer(),
-              SignWithButton(
-                onTap: () async {
-                  try {
-                    await Auth.signInWithGoogle();
-                    logger.i(auth.currentUser!.email);
-                    prefs.setBool('isUserLoggedIn', true);
-                    Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(
-                        builder: (_) => const StageScreen(),
-                      ),
-                      (route) => false,
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    logger.e(e);
-                    ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(e.message!),
-                      ),
-                    );
-                  } catch (e) {
-                    logger.e(e);
-                    ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(e.toString()),
-                      ),
-                    );
-                  }
-                },
-                size: size,
-                icon: FontAwesomeIcons.google,
-                text: 'SIGN IN WITH GOOGLE',
-              ),
-              // const SizedBox(height: 15),
-              // SignWithButton(
-              //   size: size,
-              //   icon: FontAwesomeIcons.facebook,
-              //   text: 'SIGN UP WITH FACEBOOK',
-              // ),
-              const SizedBox(height: 30),
-              AppButton(
-                text: 'GET STARTED',
-                onTap: () =>
-                    Navigator.of(context).pushNamed(SignUpScreen.routeName),
-              ),
-              const SizedBox(height: 15),
-              GestureDetector(
-                onTap: () =>
-                    Navigator.of(context).pushNamed(LoginScreen.routeName),
-                child: const Text(
-                  'Have an account? SIGN IN',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
+              width: size.width,
+              height: size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.2),
+                    BlendMode.darken,
                   ),
+                  image: const AssetImage('assets/images/bg4.jpg'),
                 ),
               ),
-              const SizedBox(height: 30),
-            ],
-          ),
-        ),
-      ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 25, horizontal: 45),
+                child: Column(
+                  children: [
+                    const Spacer(),
+                    SignWithButton(
+                      onTap: () async {
+                        try {
+                          await Auth.signInWithGoogle();
+                          logger.i(auth.currentUser!.email);
+                          prefs.setBool('isUserLoggedIn', true);
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (_) => const StageScreen(),
+                            ),
+                            (route) => false,
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          logger.e(e);
+                          ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("An error has occurred"),
+                            ),
+                          );
+                        } catch (e) {
+                          logger.e(e);
+                          ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("An error has occurred"),
+                            ),
+                          );
+                        }
+                      },
+                      size: size,
+                      icon: FontAwesomeIcons.google,
+                      text: 'SIGN IN WITH GOOGLE',
+                    ),
+                    // const SizedBox(height: 15),
+                    // SignWithButton(
+                    //   size: size,
+                    //   icon: FontAwesomeIcons.facebook,
+                    //   text: 'SIGN UP WITH FACEBOOK',
+                    // ),
+                    const SizedBox(height: 30),
+                    AppButton(
+                      text: 'GET STARTED',
+                      onTap: () => Navigator.of(context)
+                          .pushNamed(SignUpScreen.routeName),
+                    ),
+                    const SizedBox(height: 15),
+                    GestureDetector(
+                      onTap: () => Navigator.of(context)
+                          .pushNamed(LoginScreen.routeName),
+                      child: const Text(
+                        'Have an account? SIGN IN',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                  ],
+                ),
+              ),
+            )
+          
     );
   }
 }
